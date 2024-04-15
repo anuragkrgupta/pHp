cfbe9ef398fb2e5d39b2d296d3f933ab7f959cd1
Anurag kumar <114020159+anuragkrgupta@users.noreply.github.com>
2024-04-15 19:10:02 +0530

    Add files via upload

---
+++ var1.php
@@ -0,0 +1,6 @@
+ <!-- variable -->
+ <?php
+ $a1=10;
+echo "The variable is $a1";
+echo "<br>The variable value again ".$a1;
+ ?>
---
+++ var2.php
@@ -0,0 +1,8 @@
+<?php
+$var1=true;
+$var2=false;
+//&&
+$rs1=($var1||$var2);
+echo "Result of && is:";
+echo var_dump(!$rs1)
+?>
